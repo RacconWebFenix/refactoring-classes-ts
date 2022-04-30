b76b49a7da2cbd0ca5544384f0a233dbb76d294d
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-04-30 09:15:07 -0300

    Moal compontent to functional

--- src/components/ModalEditFood/index.jsx
+++ src/components/ModalEditFood/index.jsx
@@ -2,10 +2,15 @@ import { createRef } from "react";
 import { FiCheckSquare } from "react-icons/fi";
 
 import { Form } from "./styles";
-import Modal from "../Modal";
+import { Modal } from "../Modal";
 import Input from "../Input";
 
-export function ModalEditFood({ setIsOpen, handleUpdateFood, editingFood, isOpen }) {
+export function ModalEditFood({
+  setIsOpen,
+  handleUpdateFood,
+  editingFood,
+  isOpen,
+}) {
   const formRef = createRef();
 
   const handleSubmit = async (data) => {
